50091df2a39388f71d82369b51e2f58458a08fd5
T Brichetto <TaraPersonal@MacBook-Air.local>
2021-01-16 14:11:23 -0700

    contact page styling

--- src/components/Contact/Contactform.js
+++ src/components/Contact/Contactform.js
@@ -26,23 +26,26 @@ const ContactForm = () => {
     alert(result.status);
   };
   return (
-      <div className="contactBackground">
-        <form onSubmit={handleSubmit} className="contactForm">
-            <div className="form-group">
-                <label htmlFor="name">Your Name:</label>
-                <input type="text" className="form-control" id="name" required />
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Your Email:</label>
-                <input type="email" className="form-control" id="email" required />
-            </div>
-            <div className="form-group">
-                <label htmlFor="message">Message:</label>
-                <textarea className="form-control" id="message" required />
-            </div>
-            <button className="btn btn-outline-info btn-sm mt-auto" type="submit">{status}</button>
-        </form>
-    </div>
+      <section className='contactContainer'>
+        <div className="contactBackground">
+            <form onSubmit={handleSubmit} className="contactForm">
+                <div className="form-group">
+                    <label htmlFor="name">Your Name:</label>
+                    <input type="text" className="form-control" id="name" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Your Email:</label>
+                    <input type="email" className="form-control" id="email" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message:</label>
+                    <textarea className="form-control" id="message" required />
+                </div>
+                <button className="btn btn-outline-info btn-sm mt-auto" type="submit">{status}</button>
+            </form>
+        </div>
+        <p class="photoCredit"><a href="https://www.pexels.com/photo/close-up-photo-of-three-cactus-1856437/">Photo Credit</a></p>
+    </section>
   );
 };
 
--- src/components/Contact/Contactheader.js
+++ src/components/Contact/Contactheader.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './contact.css'
 
 function Contactheader() {
   return (
-    <section>
-      <h1 id="about">Contact Me</h1>
+    <section className="jumbotron text-center contactHeader">
+        <h1 className="display-3">Contact Me</h1>
+        <p className="lead">Open to career opportunities, freelance work, and collaborative projects.</p>
     </section>
   );
 }
