19d56843908a866317c03205a06ccd6bf26d76d8
T Brichetto <TaraPersonal@MacBook-Air.local>
2021-04-07 12:19:23 -0700

    email

--- src/components/Contact/Contactheader.js
+++ src/components/Contact/Contactheader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './contact.css'
 
+
 function Contactheader() {
   return (
     <section className="jumbotron text-center contactHeader">
         <h1 className="display-3">Contact Me</h1>
         <p className="lead">Open to career opportunities, freelance work, and collaborative projects.</p>
+        <p>Please connect with me by submitting the form below or via email at tara.brichetto@gmail.com</p>
     </section>
   );
 }
